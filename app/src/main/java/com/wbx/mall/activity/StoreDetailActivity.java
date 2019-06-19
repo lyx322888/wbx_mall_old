70398270a1373cf5afb961fb240ee7d8991a7064
Xyf6 <15159862589@163.com>
2019-06-19 15:15:16 +0800

    回复

--- app/src/main/java/com/wbx/mall/activity/StoreDetailActivity.java
+++ app/src/main/java/com/wbx/mall/activity/StoreDetailActivity.java
@@ -88,9 +88,7 @@ import butterknife.OnClick;
 
 import static android.view.View.GONE;
 
-/**
- * 测试3
- */
+
 public class StoreDetailActivity extends BaseActivity implements AddWidget.OnAddClick, GoodsFreeView {
     public static final String IS_VEGETABLE_MARKET = "IS_VEGETABLE_MARKET";
     public static final String STORE_ID = "STORE_ID";
